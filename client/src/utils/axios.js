e59597264a9e52b046605620dcdab092b1bd7ca8
Saksham <guptasaksham795@gmail.com>
2026-06-20 23:29:41 +0530

    updated

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://eventora-lalq.onrender.com/api',
+     baseURL: 'https://eventora-server-8qlu.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
